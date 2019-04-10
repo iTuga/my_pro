dbebc2151f6df049c4a2fcb0f10b9b758341c427
Roman <ddaweedd@gmail.com>
2019-04-10 10:09:05 +0000

    Message

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-const message = document.querySelector('.message button');
-message.addEventListener('click', sendMessage);
-
-
-function sendMessage(e) {
-    e.preventDefault();
-    let userMessage = prompt('Напишіть ваше повідомлення');
-}
-
 const pictures = document.querySelector('.foto-objects img');
 pictures.addEventListener('mouseover', bigPictures);
 
